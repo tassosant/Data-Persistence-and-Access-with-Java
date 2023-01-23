d51a2eaaba3efaf4165d7d07bcf786cfc4c7feec
Tasos Antoniou <tasos.an.antoniou@gmail.com>
2023-01-23 21:37:55 +0200

    first commit

---
+++ src/main/java/com/example/chinookddd/ChinookDddApplication.java
@@ -0,0 +1,13 @@
+package com.example.chinookddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinookDddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChinookDddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chinookddd/ChinookDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chinookddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChinookDddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
